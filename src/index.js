71d858041fe0545d7847945303f6de1b65e23946
Ian Hess <ian.h.hess@gmail.com>
2022-11-22 10:49:57 -0500

    work through webpack tutorial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import _ from "lodash";
+// import myName from "./myName.js";
 
-function component() {
-  const element = document.createElement("div");
+// function component() {
+//   const element = document.createElement("div");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  return Element;
-}
+//   element.textContent = myName("Cody");
 
-document.body.appendChild(component());
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+var moment = require("moment");
+
+console.log("Hello from JavaScript");
+console.log(moment().startOf("day").fromNow());
